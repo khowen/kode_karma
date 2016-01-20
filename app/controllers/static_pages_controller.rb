dceba520fcbe4ff3fe547d7b8f13a23ad371d21e
Katie Owen <katherinehowen@gmail.com>
2016-01-19 20:15:42 -0500

    Test commit for heroku deployment.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,14 @@
 class StaticPagesController < ApplicationController
-    def home
-    if user_signed_in?
-      puts "--------------YES------------------"
-      @user = User.find_by(params[:meta])
-      puts "------------------#{@user[:meta_type]}---------"
-      if @user[:meta_type] == 'Contributor'
-        redirect_to contributor_path(@user[:meta_id])
-      elsif @user[:meta_type] == 'Organization'
-        redirect_to organization_path(@user[:meta_id])
-      end
-    end
-  end
+  #   def home
+  #   if user_signed_in?
+  #     puts "--------------YES------------------"
+  #     @user = User.find_by(params[:meta])
+  #     puts "------------------#{@user[:meta_type]}---------"
+  #     if @user[:meta_type] == 'Contributor'
+  #       redirect_to contributor_path(@user[:meta_id])
+  #     elsif @user[:meta_type] == 'Organization'
+  #       redirect_to organization_path(@user[:meta_id])
+  #     end
+  #   end
+  # end
 end

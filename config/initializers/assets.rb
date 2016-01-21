b700f43900ff925b1ab81696409d0b1b5c5aadf2
Katie Owen <katherinehowen@gmail.com>
2016-01-21 14:41:13 -0500

    Updated order of stylesheets and changed application.css to be .css.sass to import order of files.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( logo.png )
 Rails.application.config.assets.precompile += %w( landing.png )
 
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
